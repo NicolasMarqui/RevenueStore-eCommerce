0d2841ba6ed7008796ed29eed0e4144ae054874c
Nicolas Marqui <44384526+NicolasMarqui@users.noreply.github.com>
2019-02-23 14:24:23 -0300

    Update key.js

--- config/key.js
+++ config/key.js
@@ -1,4 +1,4 @@
 module.exports = {
-    MongoURI: process.env.MONGODB_URI || 'mongodb://admin:admin123@ds213705.mlab.com:13705/first-time-mongo',
+    MongoURI: process.env.MONGODB_URI || 'mongodb://admin1:admin123@ds213705.mlab.com:13705/first-time-mongo',
     secretOrKey: 'secret',
 }
